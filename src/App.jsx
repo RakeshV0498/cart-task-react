b87c49979d800366f414ad1a55c44f89bfb57b3b
RakeshV0498 <rockr4674@gmail.com>
2024-04-21 20:39:33 +0530

    updated components for the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Cart from "./Components/Cart";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
+      <>
+        <Header />
         <Routes>
-          <Header />
-          <Route path="/" exact>
-            Home
-          </Route>
-          <Route path="/cart" exact>
-            Cart
-          </Route>
+          <Route path="/" exact element={<Home />}></Route>
+          <Route path="/cart" exact element={<Cart />}></Route>
         </Routes>
-      </BrowserRouter>
-    </>
+      </>
+    </BrowserRouter>
   );
 }
 
