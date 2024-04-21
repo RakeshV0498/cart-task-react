8ed994cac40d99f7f00cc8738a7201d077222e0e
RakeshV0498 <rockr4674@gmail.com>
2024-04-21 20:52:52 +0530

    included Data for the project

---
+++ src/Data.js
@@ -0,0 +1,78 @@
+import image1 from "./assets/Products/iPhone-9.jpeg";
+import image2 from "./assets/Products/Iphone-X.webp";
+import image3 from "./assets/Products/Samsung-U9.jpg";
+import image4 from "./assets/Products/OPPO-F19.jpg";
+import image5 from "./assets/Products/Huawei-P30.webp";
+
+const products = [
+  {
+    id: 1,
+    title: "iPhone 9",
+    description: "An apple mobile which is nothing like apple",
+    price: 549,
+    discountPercentage: 12.96,
+    rating: 3.69,
+    stock: 94,
+    brand: "Apple",
+    category: "smartphones",
+    image: image1,
+    count: 0,
+  },
+  {
+    id: 2,
+    title: "iPhone X",
+    description:
+      "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+    price: 899,
+    discountPercentage: 17.94,
+    rating: 4.44,
+    stock: 34,
+    brand: "Apple",
+    category: "smartphones",
+    image: image2,
+    count: 0,
+  },
+  {
+    id: 3,
+    title: "Samsung Universe 9",
+    description:
+      "Samsung's new variant which goes beyond Galaxy to the Universe",
+    price: 1249,
+    discountPercentage: 15.46,
+    rating: 4.09,
+    stock: 36,
+    brand: "Samsung",
+    category: "smartphones",
+    image: image3,
+    count: 0,
+  },
+  {
+    id: 4,
+    title: "OPPOF19",
+    description: "OPPO F19 is officially announced on April 2021.",
+    price: 280,
+    discountPercentage: 17.91,
+    rating: 4.3,
+    stock: 123,
+    brand: "OPPO",
+    category: "smartphones",
+    image: image4,
+    count: 0,
+  },
+  {
+    id: 5,
+    title: "Huawei P30",
+    description:
+      "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+    price: 499,
+    discountPercentage: 10.58,
+    rating: 4.09,
+    stock: 32,
+    brand: "Huawei",
+    category: "smartphones",
+    image: image5,
+    count: 0,
+  },
+];
+
+export default products;
