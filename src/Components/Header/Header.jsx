d11333ddbc719ea29453effcf7887f7d323c459c
RakeshV0498 <rockr4674@gmail.com>
2024-04-21 20:21:08 +0530

    Included react-icons,react-bootstrap,react-router

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,35 @@
+import {
+  Badge,
+  Container,
+  Dropdown,
+  DropdownToggle,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
+import { FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand>
+            <Link to="/">MyShop Cart</Link>
+          </Navbar.Brand>
+          <Nav>
+            <Dropdown alignright>
+              <DropdownToggle variant="primary">
+                <FaShoppingCart color="white" fontSize="25px" />
+                <Badge bg="info">{10}</Badge>
+              </DropdownToggle>
+              <Dropdown.Menu style={{ minwidth: 370 }}>
+                <span style={{ padding: 10 }}>Cart is Empty</span>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
+        </Container>
+      </Navbar>
     </>
   );
 };
